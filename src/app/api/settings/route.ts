9c8eb1fcd8e60d69ecbb4ee209367d91dcd26b77
Shlok Kohli <shlokkohli11@gmail.com>
2025-04-29 21:02:29 +0530

    route for preference settings

--- src/app/api/settings/route.ts
+++ src/app/api/settings/route.ts
@@ -1,31 +1,47 @@
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/options";
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
 
     const session = await getServerSession(authOptions)
 
-    const { notificationPreference, summaryPreference } = await request.json();
+    try {
+            const { notificationPreference, summaryPreference } = await request.json();
+        
+            // first update the notification preference
+            await prisma.notification.update({
+                where: {
+                    ownerId: session?.user.id
+                },
+                data: {
+                    NotificationTime: notificationPreference
+                }
+            })
+        
+            // then update the summary preference
+            await prisma.summary.update({
+                where: {
+                    ownerId: session?.user.id
+                },
+                data: {
+                    SummaryFormat: summaryPreference
+                }
+            })
 
-    // first update the notification preference
-    await prisma.notification.update({
-        where: {
-            ownerId: session?.user.id
-        },
-        data: {
-            NotificationTime: notificationPreference
-        }
-    })
+            return NextResponse.json(
+                { message : "Preferences updated successfully"},
+                { status: 200 }
+            )
 
-    // then update the summary preference
-    await prisma.summary.update({
-        where: {
-            ownerId: session?.user.id
-        },
-        data: {
-            SummaryFormat: summaryPreference
-        }
-    })
+    } catch (error) {
+
+        return NextResponse.json(
+            { message : "Error updating notification preference"},
+            { status: 500 }
+        )
+        
+    }
 
 }
